01125f3ae0a97e0c7774da1ecfea9ed095b43cf8
Nevena Milenković <nmilenkovic@raf.rs>
2026-06-25 20:28:38 +0200

    termin 3 parametrizacija

---
+++ Termin 3/tests/test_parametrizacija.py
@@ -0,0 +1,7 @@
+# parametrizacija je tehnika kojom jedan test mozemo pokrenuti vise puta
+# sa razlicitim ulaznim podacima
+# umesto da pisemo desetak skoro identicnih test funkcija, pisemo jednu ali joj 
+# dajemo 10 setova podataka
+# Parametrizacija se koristi kada:
+# se testira ista logika sa vise razlicitih vresnoti
+# se testiraju granicne vrednosti
