43b0a722e5f4d8dff9b4cc057658ea68e81f0bc6
Nevena Milenković <nmilenkovic@raf.rs>
2026-07-03 13:43:22 +0200

    Create test_vezba_markeri.py

---
+++ Termin 4 vezbe/tests/test_vezba_markeri.py
@@ -0,0 +1,20 @@
+import pytest
+from src.biblioteka import Biblioteka,Knjiga
+
+# fixtures su u conftest.py
+
+
+# GRUPA 1 - smoke testovi
+# najosnovniji testovi, da li app uopste radi
+# ako smoke testovi padnu nema smisla pisati ostale ili pokretati
+
+# Zadatak 1 G1
+# Markirati test sa smoke
+# Napisati test koj proverava da se Biblioteka moze klreirati sa zadatim nazivom
+
+# Zadatak 2 G1
+# Markirati test sa smoke
+# Napisati test koji proverava da se Knjiga moze dodati u biblioteku i da broj_knjiga() vraca 1 
+
+# Zadatak 3 G1
+# Markirati test sa smoke
