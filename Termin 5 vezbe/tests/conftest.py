15a1c0dc6330db186a413bf245fd3cccfcb27097
Nevena Milenković <nmilenkovic@raf.rs>
2026-07-05 22:54:51 +0200

    Update conftest.py

--- Termin 5 vezbe/tests/conftest.py
+++ Termin 5 vezbe/tests/conftest.py
@@ -1,14 +1,13 @@
 # Zadatak 1 G1
-# Napravi tri fixture-a:
+# Napraviti tri fixture-a:
 #
 #   lazni_sms — MagicMock sa spec=SmsServis
 #   lazno_placanje — MagicMock sa spec=PlacanjeServis
 #   prodavnica — ProdavnicaServis koji koristi lazni_sms i lazno_placanje
-# Hint: isto kao lazni_email i servis iz lekcije
 
 
 # Zadatak 2 G1
-# Napravi fixture prodavnica_sa_proizvodom koja:
+# Napraviti fixture prodavnica_sa_proizvodom koja:
 #- koristi fixture prodavnica
 #- dodaje jedan proizvod: naziv="Slusalice", cena=3500.0, kolicina=10
 #- vraca tuple (prodavnica, proizvod_id)
